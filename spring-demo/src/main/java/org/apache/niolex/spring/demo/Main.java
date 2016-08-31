cea74c12daf50e51e662fb003ced01793fa4f92d
Lex <pftxie@qq.com>
2016-08-31 09:43:59 +0800

    Using YAML to config application.

--- spring-demo/src/main/java/org/apache/niolex/spring/demo/Main.java
+++ spring-demo/src/main/java/org/apache/niolex/spring/demo/Main.java
@@ -3,15 +3,15 @@ package org.apache.niolex.spring.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Hello world!
  *
  */
+// We use application.yml, which is loaded by spring boot by default.
+// @PropertySource(value = { "classpath:config.properties" })
 @SpringBootApplication
-@PropertySource(value = { "classpath:config.properties" })
 public class Main {
 
     @Bean
@@ -19,8 +19,21 @@ public class Main {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
+    /**
+     * The way to load yaml manually.
+     * 
+     * @Bean
+     *       public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+     *       PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+     *       YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+     *       yaml.setResources(new ClassPathResource("config.yaml"));
+     *       configurer.setProperties(yaml.getObject());
+     *       return configurer;
+     *       }
+     */
+
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "prod");
+        System.setProperty("spring.profiles.active", "dev");
         SpringApplication.run(Main.class, args);
     }
 
