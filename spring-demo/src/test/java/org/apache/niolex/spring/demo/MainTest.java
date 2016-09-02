79e190f7ace9c041fe1a9f0506b0e5c52202a429
Lex <pftxie@qq.com>
2016-09-02 18:02:42 +0800

    Add integration tests.

--- spring-demo/src/test/java/org/apache/niolex/spring/demo/MainTest.java
+++ spring-demo/src/test/java/org/apache/niolex/spring/demo/MainTest.java
@@ -1,6 +1,7 @@
 package org.apache.niolex.spring.demo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -10,13 +11,16 @@ import org.apache.niolex.commons.net.HTTPResult;
 import org.apache.niolex.commons.net.NetException;
 import org.apache.niolex.commons.net.RESTClient;
 import org.apache.niolex.commons.net.RESTResult;
+import org.apache.niolex.commons.test.OrderedRunner;
 import org.apache.niolex.spring.demo.vo.User;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(OrderedRunner.class)
 public class MainTest {
 
     RESTClient client = new RESTClient("http://localhost:8080", "utf8", 1000, 1000);
@@ -54,5 +58,75 @@ public class MainTest {
         User r = result.getResponse();
 
         assertEquals("CEO", r.getPosition());
+        assertEquals(1, r.getUserId());
     }
+
+    @Test
+    public void testPost2() throws NetException, IOException {
+        User params = new User();
+        params.setUserName("Andy");
+        params.setPosition("SSE");
+        RESTResult<User> result = client.post("users", User.class, params);
+        assertEquals(201, result.getRespCode());
+        User r = result.getResponse();
+
+        assertEquals("SSE", r.getPosition());
+        assertEquals(2, r.getUserId());
+    }
+
+    @Test
+    public void testRetrieve() throws NetException, IOException {
+        RESTResult<User> result = client.get("/users/1", User.class, null);
+        assertEquals(200, result.getRespCode());
+        User r = result.getResponse();
+
+        assertEquals("CEO", r.getPosition());
+        assertEquals("Lex", r.getUserName());
+        assertEquals(1, r.getUserId());
+    }
+
+    @Test
+    public void testRetrieveUpdate() throws NetException, IOException {
+        User params = new User();
+        params.setUserName("Lily");
+        params.setPosition("CFO");
+
+        RESTResult<User> result = client.put("/users/1", User.class, params);
+        assertEquals(200, result.getRespCode());
+        User r = result.getResponse();
+
+        assertEquals("CEO", r.getPosition());
+        assertEquals(1, r.getUserId());
+    }
+
+    @Test
+    public void testRetrieveZZZ() throws NetException, IOException {
+        RESTResult<User> result = client.get("/users/1", User.class, null);
+        assertEquals(200, result.getRespCode());
+        User r = result.getResponse();
+
+        assertEquals("CFO", r.getPosition());
+        assertEquals("Lily", r.getUserName());
+        assertEquals(1, r.getUserId());
+    }
+
+    @Test
+    public void testZDel() throws NetException, IOException {
+        RESTResult<User> result = client.delete("/users/2", User.class, null);
+
+        assertEquals(200, result.getRespCode());
+        User r = result.getResponse();
+
+        assertEquals("SSE", r.getPosition());
+        assertEquals("Andy", r.getUserName());
+        assertEquals(2, r.getUserId());
+    }
+
+    @Test
+    public void testZRetrieve() throws NetException, IOException {
+        HTTPResult result = client.get("/users/2", null);
+        assertEquals(200, result.getRespCode());
+        assertNull(result.getRespBody());
+    }
+
 }
