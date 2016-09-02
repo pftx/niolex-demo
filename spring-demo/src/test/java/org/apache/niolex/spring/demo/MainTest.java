70ea0fcf76a4ab50ebda988a1a30f4d2ace25a90
Lex <pftxie@qq.com>
2016-09-02 17:15:24 +0800

    Add more integration-test cases.

--- spring-demo/src/test/java/org/apache/niolex/spring/demo/MainTest.java
+++ spring-demo/src/test/java/org/apache/niolex/spring/demo/MainTest.java
@@ -1,38 +1,58 @@
 package org.apache.niolex.spring.demo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.niolex.commons.net.HTTPResult;
+import org.apache.niolex.commons.net.NetException;
+import org.apache.niolex.commons.net.RESTClient;
+import org.apache.niolex.commons.net.RESTResult;
+import org.apache.niolex.spring.demo.vo.User;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class MainTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MainTest( String testName )
-    {
-        super( testName );
+public class MainTest {
+
+    RESTClient client = new RESTClient("http://localhost:8080", "utf8", 1000, 1000);
+
+    @BeforeClass
+    public static void setUp() {
+        Main.main(new String[] { "hello" });
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MainTest.class );
+    @Test
+    public void testGet() throws NetException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", "Lex");
+        HTTPResult result = client.get("users", params);
+
+        assertEquals("We are in dev, Lex!", result.getRespBodyStr());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testGet2() throws NetException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", "Andy");
+        HTTPResult result = client.get("users", params);
+
+        assertEquals("We are in dev, Andy!", result.getRespBodyStr());
+    }
+
+    @Test
+    public void testPost() throws NetException, IOException {
+        User params = new User();
+        params.setUserName("Lex");
+        params.setPosition("CEO");
+        RESTResult<User> result = client.post("users", User.class, params);
+        assertEquals(201, result.getRespCode());
+        User r = result.getResponse();
+
+        assertEquals("CEO", r.getPosition());
     }
 }
